7a0e153b9a0aff261de4d97e8af9d5f346455552
Vansh Jain <vanshcodes3@gmail.com>
2026-03-20 18:42:02 +0530

    fix marketing layout

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-export const dynamic = 'force-dynamic';
-
 import { HeroSection } from '@/components/sections/HeroSection';
 import { USPSection } from '@/components/sections/USPSection';
 import { LifeJourneySection } from '@/components/sections/LifeJourneySection';
